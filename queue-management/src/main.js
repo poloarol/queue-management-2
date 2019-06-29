00f9c9abcc0d5b49db96ef3148c9216fa594372f
Polo Wambo <polo@Mac-mini-2.local>
2019-06-28 21:17:12 -0400

    Semantic-UI and axios setup

--- queue-management/src/main.js
+++ queue-management/src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
+import SuiVue from 'semantic-ui-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './components/App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import 'semantic-ui-css/semantic.min.css'
+import './assets/style.css'
+
 Vue.config.productionTip = false
+
+Vue.use(SuiVue)
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
