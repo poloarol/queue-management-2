1f85def84bccb777acdc80443dc6f911e0df6086
polo <polo@4201-GRJ6XM2-19.uottawa.o.univ>
2019-11-25 15:42:52 -0500

    changes

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -13,6 +13,7 @@ router.get('/', async(req, res) => {
         const connection = await Pool.getConnection()
         const query = 'select job.id as id, job.fname as name, job.software as software, job.station_id as post, job.faculty_id as f_id from lab2019_scheduler.job where job.assisted = false'
         let result = await connection.query(query)
+        console.log(result)
         res.send(result)
         connection.done()
     }catch(err){
